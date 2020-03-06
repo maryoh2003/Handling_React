8163e36fd60d0ff9d94505a8519758a3caeb723e
하형 <51150005+maryoh2003@users.noreply.github.com>
2020-03-07 00:49:06 +0900

    [ADD] Chapter 21
    백엔드 프로그래밍: Node.js의 Koa 프레임워크 마무리

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,9 +1,12 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
-
+/*
 //라우터 설정
 router.get('/', ctx => {
   ctx.body = '홈';
@@ -21,11 +24,18 @@ router.get('/posts', ctx => {
   ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
 });
 
+*/
 /*
 http://localhost:4000/about/react
 http://localhost:4000/posts
 http://localhost:4000/posts?id=10
 */
+
+//라우터 설정
+router.use('/api', api.routes()); //api 라우트 적용
+
+//라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
 
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
