20ab4ba8d3e2bda719ffe50208c244e65e2a8a01
하형 <51150005+maryoh2003@users.noreply.github.com>
2020-03-10 22:11:08 +0900

    [ADD] Chapter 22
    mongoose를 이용한 MongoDB 연동 실습

--- blog/blog-backend/src/index.js
+++ blog/blog-backend/src/index.js
@@ -1,47 +1,5 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser');
+//이 파일에서만 no-global-assign ESLint 옵션을 비활성화합니다.
+/* eslint-disable no-global-assign*/
 
-const api = require('./api');
-
-const app = new Koa();
-const router = new Router();
-/*
-//라우터 설정
-router.get('/', ctx => {
-  ctx.body = '홈';
-});
-
-router.get('/about/:name?', ctx => {
-  const { name } = ctx.params;
-  //name의 존재 유무에 따라 다른 결과 출력
-  ctx.body = name ? `${name}의 소개` : '소개';
-});
-
-router.get('/posts', ctx => {
-  const { id } = ctx.query;
-  //id의 존재 유무에 따라 다른 결과 출력
-  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
-});
-
-*/
-/*
-http://localhost:4000/about/react
-http://localhost:4000/posts
-http://localhost:4000/posts?id=10
-*/
-
-//라우터 설정
-router.use('/api', api.routes()); //api 라우트 적용
-
-//라우터 적용 전에 bodyParser 적용
-app.use(bodyParser());
-
-//app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-
-app.listen(4000, () => {
-  console.log('Listening to port 4000');
-});
-
-//포트를 4000번으로 열고, 서버에 접속하면 'hello world'라는 텍스트를 반환하도록 설정했다
+require = require('esm')(module /*, options*/);
+module.exports = require('./main.js');
---
+++ blog/blog-backend/src/main.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import mongoose from 'mongoose';
+
+import api from './api';
+
+const { PORT, MONGO_URL } = process.env;
+
+mongoose
+  .connect(MONGO_URL, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(e => {
+    console.error(e);
+  });
+
+const app = new Koa();
+const router = new Router();
+/*
+//라우터 설정
+router.get('/', ctx => {
+  ctx.body = '홈';
+});
+
+router.get('/about/:name?', ctx => {
+  const { name } = ctx.params;
+  //name의 존재 유무에 따라 다른 결과 출력
+  ctx.body = name ? `${name}의 소개` : '소개';
+});
+
+router.get('/posts', ctx => {
+  const { id } = ctx.query;
+  //id의 존재 유무에 따라 다른 결과 출력
+  ctx.body = id ? `포스트 #${id}` : '포스트 아이디가 없습니다.';
+});
+
+*/
+/*
+http://localhost:4000/about/react
+http://localhost:4000/posts
+http://localhost:4000/posts?id=10
+*/
+
+//라우터 설정
+router.use('/api', api.routes()); //api 라우트 적용
+
+//라우터 적용 전에 bodyParser 적용
+app.use(bodyParser());
+
+//app 인스턴스에 라우터 적용
+app.use(router.routes()).use(router.allowedMethods());
+
+//PORT가 지정되어 있지 않다면 4000을 사용
+const port = PORT || 4000;
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
+});
