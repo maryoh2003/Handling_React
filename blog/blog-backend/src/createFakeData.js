4c268c99ab6a46dd9ce057aad1f9d3349210d6ae
하형 <51150005+maryoh2003@users.noreply.github.com>
2020-03-13 20:29:02 +0900

    [UPDATE] Chapter 22
    mongoose를 이용한 MongooseDB 연동 실습 마무리

---
+++ blog/blog-backend/src/createFakeData.js
@@ -0,0 +1,16 @@
+import Post from './models/post';
+
+export default function createFakeData() {
+  // 0, 1, ... 39로 이루어진 배열을 생성한 후 포스트 데이터로 변환
+  const posts = [...Array(40).keys()].map(i => ({
+    title: `포스트 #${i}`,
+    // https://www.lipsu.com/에서 복사한 200자 이상의 텍스트
+    body:
+      'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat',
+    tags: ['가짜', '데이터'],
+  }));
+
+  Post.insertMany(posts, (err, docs) => {
+    console.log(docs);
+  });
+}
