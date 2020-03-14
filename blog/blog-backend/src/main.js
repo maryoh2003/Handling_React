526afcd53a39ea40c504fbcfcd1369cfd2f083fd
하형 <51150005+maryoh2003@users.noreply.github.com>
2020-03-14 21:57:35 +0900

    [ADD] Chapter 23
    JWT를 통한 회원 인증 시스템 구현하기 시작

--- blog/blog-backend/src/main.js
+++ blog/blog-backend/src/main.js
@@ -5,6 +5,7 @@ import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
 import api from './api';
+import jwtMiddleware from './lib/jwtMiddleware';
 import createFakeData from './createFakeData';
 
 const { PORT, MONGO_URL } = process.env;
@@ -50,6 +51,7 @@ router.use('/api', api.routes()); //api 라우트 적용
 
 //라우터 적용 전에 bodyParser 적용
 app.use(bodyParser());
+app.use(jwtMiddleware);
 
 //app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
